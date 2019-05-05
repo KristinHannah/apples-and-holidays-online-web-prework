906dae552023bee513b30658c8f62b64836cdc11
Kristin Kraemer <kristinhannahk@gmail.com>
2019-05-05 19:20:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -104,7 +104,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supply|
      binding.pry
-    if holiday.key("BBQ") 
+    if supply == "BBQ"
       bbq_include << holiday
   end
   return bbq_include
